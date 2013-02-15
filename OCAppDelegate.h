861e9440169e51587299991fbd3e88b2b43a7bca
James Carlson <jxxcarlson@mac.com>
2013-02-15 06:02:44 -0800

    Create OCAppDelegate.h

---
+++ OCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OCAppDelegate.h
+//  RandomWalk
+//
+//  Created by James Carlson on 2/14/13.
+//  Copyright (c) 2013 James Carlson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
