1d4354a807d9abbd80dbfbb671e54fd3f23ff136
James Carlson <jxxcarlson@mac.com>
2013-02-18 17:50:42 -0800

    Create OCDynamicViewReporter.h
    Protocol for objects which report on the status of an OCDynamicView object.

---
+++ OCDynamicViewReporter.h
@@ -0,0 +1,15 @@
+//
+//  OCDynamicViewReporter.h
+//  Dynamics
+//
+//  Created by James Carlson on 2/18/13.
+//  Copyright (c) 2013 James Carlson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol OCDynamicViewReporter <NSObject>
+
+- (void) updateStatusOfDynamicView;
+
+@end
