36a090aa692b7b534550014c617acbea6270382a
James Carlson <jxxcarlson@mac.com>
2013-02-18 21:12:23 -0500

    Update OCViewController.m

--- OCViewController.m
+++ OCViewController.m
@@ -8,6 +8,8 @@
 
 #import "OCViewController.h"
 #import "OCDynamicView.h"
+#import "OCRandomShapes.h"  // XX //
+
 
 @interface OCViewController ()
 
@@ -21,10 +23,12 @@
     
 }
 
-- (void) updateUI {
+- (void) updateStatusOfDynamicView {
     
     self.statusLabel.text = [NSString stringWithFormat:@"%d", self.dynamicView.frameCount];
-
+    
+    self.frameRateLabel.text = [NSString stringWithFormat:@"%1.0f", self.dynamicView.frameRate];
+    
 }
 
 
@@ -32,18 +36,61 @@
 {
     [super viewDidLoad];
     
-    self.dynamicView = [[OCDynamicView alloc] initWithFrame:CGRectMake(0, 0, 320, 520)];
-    
+    self.dynamicView = [[OCDynamicView alloc] initWithFrame:CGRectMake(0, 0, 320, 515)];
     [self.view addSubview:self.dynamicView];
+    self.dynamicView.reportingViewController = self;                // wire main view controller to dynamicView
+    [self setStartStopButtonTitle];
     
-    self.dynamicView.mvc = self;
-  
+    self.ds = [[OCRandomShapes alloc] init];  // set up dynamical system  // XX //
+    self.dynamicView.ds = self.ds;              // wire it to the dynamicView
+    [self.dynamicView initDynamicalSystem];     // initialize the dynamical system
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+/////////////////////////////////////////////////////////////////
+/// USER INTERACTION
+/////////////////////////////////////////////////////////////////
+
+- (void) setStartStopButtonTitle {
+    
+    if (self.dynamicView.running) {
+        [self.startStopButton setTitle: @"Stop" forState: UIControlStateNormal];
+    } else {
+        [self.startStopButton setTitle: @"Go" forState: UIControlStateNormal];
+    }
+}
+
+- (IBAction)startStop:(id)sender {
+    
+    self.dynamicView.running = !self.dynamicView.running;
+    [self setStartStopButtonTitle];
+}
+
+- (IBAction)clear:(id)sender {
+    
+    [self.dynamicView clear];
+    [self setStartStopButtonTitle];
+}
+
+
+-(IBAction)increaseFrameRate:(id)sender {
+    
+    [self.dynamicView updateFrameRate:self.dynamicView.frameRate + 1];
+    
+}
+
+-(IBAction)decreaseFrameRate:(id)sender {
+    if (self.dynamicView.frameRate > 1) {
+        [self.dynamicView updateFrameRate:self.dynamicView.frameRate - 1];
+    }
+    
 }
 
 @end
