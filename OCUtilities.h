b39b0d0755473c691bee2d9da2fa20f7cd99d217
James Carlson <jxxcarlson@mac.com>
2013-02-18 17:44:59 -0800

    Create OCUtilities.h
    Utilities (random number generators) used by Kaleidoscope

---
+++ OCUtilities.h
@@ -0,0 +1,23 @@
+//
+//  OCUtilties.h
+//  Dynamics
+//
+//  Created by James Carlson on 2/17/13.
+//  Copyright (c) 2013 James Carlson. All rights reserved.
+//
+
+#ifndef OCUtilities_h
+#define OCUtilities_h
+
+
+#endif
+
+// return random number in unit interval [0,1]
+float urandom();
+
+// As in Processing, map a from the interval [a,b] affinely to the interval [c,d]
+float map( float x, float a, float b, float c, float d);
+
+// return random number interval [a,b]
+float IRandom(float a, float b);
+
