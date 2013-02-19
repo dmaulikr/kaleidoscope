5611513181ea29938d1e703905916b9722bad52a
James Carlson <jxxcarlson@mac.com>
2013-02-18 18:14:15 -0800

    Create OCRandomShapes.h
    Defines class to draw triangles of random shape, size, and color.
    Implements the OCDynamicalSystem protocol.

---
+++ OCRandomShapes.h
@@ -0,0 +1,24 @@
+//
+//  OCRandomShapes.h
+//  Dynamics
+//
+//  Created by James Carlson on 2/17/13.
+//  Copyright (c) 2013 James Carlson. All rights reserved.
+//
+//
+//  OCDynamicalSystem.h
+//  Kaleidoscope
+//
+//  Created by James Carlson on 2/16/13.
+//  Copyright (c) 2013 James Carlson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OCDynamicalSystem.h"
+
+@interface OCRandomShapes : NSObject <OCDynamicalSystem>
+
+@property (assign) int frameWidth, frameHeight;
+
+@end
+
